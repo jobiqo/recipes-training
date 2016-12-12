95bd8fb9b789ae88f77807cdf138245c51105703
Klaus Purer <klaus.purer@gmail.com>
2016-12-12 11:43:53 +0100

    Add settings form stub

---
+++ mymodule/src/Form/MymoduleSettingsForm.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\mymodule\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure mymodule settings for this site.
+ */
+class MymoduleSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'maymodule_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['mymodule.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Insert the form elements here!
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Insert saving of the configuration here!
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
