573ecccd8f64e79a9b14d825c8f18c6769c641cb
Klaus Purer <klaus.purer@gmail.com>
2016-12-12 11:14:59 +0100

    Add stub for site name block

---
+++ mymodule/src/Plugin/Block/SiteName.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\recipes\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a block that show the sitename.
+ *
+ * Insert the block annotation here!
+ *
+ * 
+ */
+class SiteName extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Create the block contents here!
+  }
+
+}
